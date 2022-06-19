b3213a57b5984f68ab3f6ae1ef90f095ac3fb8ed
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-19 12:58:07 +0300

    commit tree 537ca2250fcc9a7ee9e22801c0207cf39eb1ad7a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function createEmployeeRecord(detail){
+const employeeDetails = {
+    firstName: detail[0],
+    familyName: detail[1],
+    title: detail[2],
+    payPerHour: detail[3],
+    timeInEvents: [],
+    timeOutEvents: []
+}
+return employeeDetails;
+
+}
